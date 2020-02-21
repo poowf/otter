fb3a53f42d253db01b8c3e12800a54c67e93ea07
Zach DeCook <zachdecook@librem.one>
2020-02-21 05:44:48 -0500

    * Navigation View: Rename to be more semantic

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
             @include("otter::partials/header")
         </header>
         <nav>
-            @include("otter::partials/sidebar")
+            @include("otter::partials/nav")
         </nav>
         <section id="app">
             @yield("content")
